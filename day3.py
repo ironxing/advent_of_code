93b33c47e59f14dc8235852759dc8dbf2071eb64
ironxing <liuxing77777@gmail.com>
2018-08-20 17:35:14 +0200

    day3_1

---
+++ day3.py
@@ -0,0 +1,27 @@
+import math
+
+
+def cal_n(number):
+    root_number = math.ceil(number**0.5)
+
+    if root_number % 2 == 0:
+        return root_number + 1
+    else:
+        return root_number
+
+
+number = 28
+
+n = cal_n(number)
+
+coor_1_1 = int((n-1)/2+1)
+
+location = math.floor((n**2 - number)/(n-1))
+
+
+
+
+
+print(n)
+print(coor_1_1)
+print(location)
